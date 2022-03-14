c2ff7b2a5df5dfdd6e839f037bcc72ea2eb2fadf
ambroise <ambroise.berthe@ikomia.com>
2022-03-14 09:18:12 +0100

    Initial commit

---
+++ train_detectron2_detection_process.py
@@ -0,0 +1,240 @@
+# Copyright (C) 2021 Ikomia SAS
+# Contact: https://www.ikomia.com
+#
+# This file is part of the IkomiaStudio software.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from ikomia import core, dataprocess
+from ikomia.dnn import datasetio, dnntrain
+from ikomia.core.task import TaskParam
+# Your imports below
+from datetime import datetime
+# Setup detectron2 logger
+import detectron2
+import torch
+from detectron2.utils.logger import setup_logger
+
+setup_logger()
+import os
+import copy
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.config import get_cfg, CfgNode
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from train_detectron2_detection.utils import MyMapper, MyTrainer, register_datasets
+import gc
+
+
+# --------------------
+# - Class to handle the process parameters
+# - Inherits PyCore.CWorkflowTaskParam from Ikomia API
+# --------------------
+class TrainDetectron2DetectionParam(TaskParam):
+
+    def __init__(self):
+        TaskParam.__init__(self)
+        # Place default value initialization here
+        # Example : self.windowSize = 25
+        self.cfg["model_name"] = "COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"
+        self.cfg["custom_cfg"] = False
+        self.cfg["cfg_path"] = ""
+        self.cfg["max_iter"] = 100
+        self.cfg["batch_size"] = 2
+        self.cfg["input_size"] = 400
+        self.cfg["pretrained"] = True
+        self.cfg["output_folder"] = os.path.dirname(__file__) + "/runs"
+        self.cfg["learning_rate"] = 0.0025
+        self.cfg["split_train_test"] = 0.8
+        self.cfg["eval_period"] = 50
+
+    def setParamMap(self, param_map):
+        # Set parameters values from Ikomia application
+        # Parameters values are stored as string and accessible like a python dict
+        # Example : self.windowSize = int(param_map["windowSize"])
+        self.cfg["model_name"] = param_map["model_name"]
+        self.cfg["custom_cfg"] = eval(param_map["custom_cfg"])
+        self.cfg["cfg_path"] = param_map["cfg_path"]
+        self.cfg["max_iter"] = int(param_map["max_iter"])
+        self.cfg["batch_size"] = int(param_map["batch_size"])
+        self.cfg["input_size"] = int(param_map["input_size"])
+        self.cfg["pretrained"] = eval(param_map["pretrained"])
+        self.cfg["output_folder"] = param_map["output_folder"]
+        self.cfg["learning_rate"] = float(param_map["learning_rate"])
+        self.cfg["split_train_test"] = float(param_map["split_train_test"])
+        self.cfg["eval_period"] = int(param_map["eval_period"])
+
+    def getParamMap(self):
+        # Send parameters values to Ikomia application
+        # Create the specific dict structure (string container)
+        param_map = core.ParamMap()
+        # Example : paramMap["windowSize"] = str(self.windowSize)
+        param_map["model_name"] = self.cfg["model_name"]
+        param_map["custom_cfg"] = str(self.cfg["custom_cfg"])
+        param_map["cfg_path"] = self.cfg["cfg_path"]
+        param_map["max_iter"] = str(self.cfg["max_iter"])
+        param_map["batch_size"] = str(self.cfg["batch_size"])
+        param_map["input_size"] = str(self.cfg["input_size"])
+        param_map["pretrained"] = str(self.cfg["pretrained"])
+        param_map["output_folder"] = self.cfg["output_folder"]
+        param_map["learning_rate"] = str(self.cfg["learning_rate"])
+        param_map["split_train_test"] = str(self.cfg["split_train_test"])
+        param_map["eval_period"] = str(self.cfg["eval_period"])
+        return param_map
+
+
+# --------------------
+# - Class which implements the process
+# - Inherits PyCore.CWorkflowTask or derived from Ikomia API
+# --------------------
+class TrainDetectron2Detection(dnntrain.TrainProcess):
+
+    def __init__(self, name, param):
+        dnntrain.TrainProcess.__init__(self, name, param)
+        self.stop_train = False
+        self.epochs_done = None
+        self.epochs_todo = None
+        # Percentage of training done for display purpose
+        self.advancement = 0
+        # Create parameters class
+        if param is None:
+            self.setParam(TrainDetectron2DetectionParam())
+        else:
+            self.setParam(copy.deepcopy(param))
+
+    def getProgressSteps(self, eltCount=1):
+        # Function returning the number of progress steps for this process
+        # This is handled by the main progress bar of Ikomia application
+        return 100
+
+    def update_progress(self):
+        self.epochs_done += 1
+        steps = range(self.advancement, int(100 * self.epochs_done / self.epochs_todo))
+        for step in steps:
+            self.emitStepProgress()
+            self.advancement += 1
+
+    def run(self):
+        # Core function of your process
+        # Call beginTaskRun for initialization
+        self.beginTaskRun()
+        self.stop_train = False
+        gc.collect()
+        torch.cuda.empty_cache()
+
+        ik_dataset = self.getInput(0)
+        str_datetime = datetime.now().strftime("%d-%m-%YT%Hh%Mm%Ss")
+
+        # Get parameters :
+        param = self.getParam()
+
+        tb_logdir = self.getTensorboardLogDir() + "/" + param.cfg["model_name"] + "/" + str_datetime
+
+        if not param.cfg["custom_cfg"]:
+            out_dir = param.cfg["output_folder"] + "/" + str_datetime
+
+            cfg = get_cfg()
+            config_path = os.path.join(os.path.dirname(detectron2.__file__), "model_zoo", "configs",
+                                       param.cfg["model_name"] + '.yaml')
+            cfg.merge_from_file(config_path)
+            # Find a model from detectron2's model zoo. You can use the https://dl.fbaipublicfiles... url as well
+            if not param.cfg["pretrained"]:
+                cfg.MODEL.WEIGHTS = None # model_zoo.get_checkpoint_url(param.cfg["model_name"] + ".yaml")
+            cfg.INPUT.MAX_SIZE_TEST = param.cfg["input_size"]
+            cfg.INPUT.MAX_SIZE_TRAIN = param.cfg["input_size"]
+            cfg.SOLVER.IMS_PER_BATCH = param.cfg["batch_size"]
+            cfg.SOLVER.BASE_LR = param.cfg["learning_rate"]
+            cfg.SOLVER.MAX_ITER = param.cfg["max_iter"]
+            cfg.TEST.EVAL_PERIOD = min(param.cfg["eval_period"], cfg.SOLVER.MAX_ITER - 1)
+            cfg.SOLVER.STEPS = (int(0.8 * param.cfg["max_iter"]), int(0.9 * param.cfg["max_iter"]))
+            cfg.SOLVER.WARMUP_ITERS = 1000
+            cfg.DATALOADER.NUM_WORKERS = 0
+            cfg.OUTPUT_DIR = out_dir
+
+        else:
+            if os.path.isfile(param.cfg["cfg_path"]):
+                with open(param.cfg["cfg_path"], 'r') as file:
+                    cfg_data = file.read()
+                    cfg = CfgNode.load_cfg(cfg_data)
+                    out_dir = cfg.OUTPUT_DIR
+            else:
+                print("Unable to load config file {}".format(param.cfg["cfg_path"]))
+                self.endTaskRun()
+
+        os.makedirs(out_dir, exist_ok=True)
+
+        # Fixed dataset names
+        cfg.DATASETS.TRAIN = ("TrainDetectionDataset",)
+        cfg.DATASETS.TEST = ("TestDetectionDataset",)
+        register_datasets(ik_dataset.data, param.cfg["split_train_test"])
+        cfg.CLASS_NAMES = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_classes")
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = len(cfg.CLASS_NAMES)
+        cfg.MODEL.RETINANET.NUM_CLASSES = len(cfg.CLASS_NAMES)
+        thing_dataset_id_to_contiguous_id = MetadataCatalog.get(cfg.DATASETS.TRAIN[0]).get("thing_dataset_id_to_contiguous_id")
+        self.advancement = 0
+        self.epochs_todo = cfg.SOLVER.MAX_ITER
+        self.epochs_done = 0
+        with open(os.path.join(out_dir, "config.yaml"), 'w') as f:
+            f.write(cfg.dump())
+        trainer = MyTrainer(cfg, tb_logdir, out_dir, self.get_stop_train, self.log_metrics, self.update_progress, thing_dataset_id_to_contiguous_id)
+        if param.cfg["pretrained"]:
+            trainer.resume_or_load(resume=False)  # load MODEL.WEIGHTS
+        trainer.train()
+
+        del trainer
+        gc.collect()
+        torch.cuda.empty_cache()
+        # Call endTaskRun to finalize process
+        self.endTaskRun()
+
+    def get_stop_train(self):
+        return self.stop_train
+
+    def stop(self):
+        super(TrainDetectron2Detection, self).stop()
+        self.stop_train = True
+
+
+# --------------------
+# - Factory class to build process object
+# - Inherits PyDataProcess.CTaskFactory from Ikomia API
+# --------------------
+class TrainDetectron2DetectionFactory(dataprocess.CTaskFactory):
+
+    def __init__(self):
+        dataprocess.CTaskFactory.__init__(self)
+        # Set process information as string here
+        self.info.name = "train_detectron2_detection"
+        self.info.shortDescription = "your short description"
+        self.info.description = "your description"
+        # relative path -> as displayed in Ikomia application process tree
+        self.info.path = "Plugins/Python/Train"
+        self.info.version = "1.0.0"
+        # self.info.iconPath = "your path to a specific icon"
+        self.info.authors = "algorithm author"
+        self.info.article = "title of associated research article"
+        self.info.journal = "publication journal"
+        self.info.year = 2021
+        self.info.license = "MIT License"
+        # URL of documentation
+        self.info.documentationLink = ""
+        # Code source repository
+        self.info.repository = ""
+        # Keywords used for search
+        self.info.keywords = "your,keywords,here"
+
+    def create(self, param=None):
+        # Create process object
+        return TrainDetectron2Detection(self.info.name, param)
---
+++ utils.py
@@ -0,0 +1,183 @@
+import detectron2.data.transforms as T
+from detectron2.data import DatasetMapper
+from detectron2.data import build_detection_train_loader, build_detection_test_loader
+import copy
+from detectron2.data import detection_utils as utils
+from typing import List, Union
+import logging
+# import some common detectron2 utilities
+from detectron2.engine import DefaultTrainer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.structures import BoxMode
+from detectron2.evaluation import COCOEvaluator, verify_results
+from detectron2.utils.events import TensorboardXWriter
+from detectron2.engine.hooks import BestCheckpointer
+from detectron2.utils import comm
+from detectron2.utils.events import EventStorage
+import os
+import torch
+import random
+
+
+class MyTrainer(DefaultTrainer):
+    tensorboard_dir = ""
+    output_dir = ""
+    eval_period = 0
+    thing_dataset_id_to_contiguous_id = None
+
+    def __init__(self, cfg, tb_dir, output_dir, stop_train, log_metrics, update_progress, thing_dataset_id_to_contiguous_id):
+        super().__init__(cfg)
+        MyTrainer.tensorboard_dir = tb_dir
+        MyTrainer.output_dir = output_dir
+        MyTrainer.eval_period = cfg.TEST.EVAL_PERIOD
+        MyTrainer.thing_dataset_id_to_contiguous_id = thing_dataset_id_to_contiguous_id
+        self.stop_train = stop_train
+        self.log_metrics = log_metrics
+        self.update_progress = update_progress
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        return build_detection_train_loader(cfg, mapper=MyMapper(True, augmentations=[
+            T.RandomBrightness(0.9, 1.1),
+            T.RandomFlip(prob=0.5),
+            T.ResizeShortestEdge(short_edge_length=cfg.INPUT.MAX_SIZE_TRAIN, max_size=cfg.INPUT.MAX_SIZE_TRAIN)
+        ], image_format="RGB"))
+
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name):
+        return COCOEvaluator(dataset_name, distributed=False, output_dir=cls.output_dir)
+
+    @classmethod
+    def build_writers(cls):
+        return [TensorboardXWriter(cls.tensorboard_dir)]
+
+    def build_hooks(self):
+        ret = super().build_hooks()
+        ret.append(BestCheckpointer(eval_period=self.eval_period,
+                                    checkpointer=self.checkpointer,
+                                    val_metric="bbox/AP",
+                                    mode="max",
+                                    file_prefix="model_best"))
+        return ret
+
+    def train(self):
+        logger = logging.getLogger(__name__)
+        logger.info("Starting training from iteration {}".format(self.start_iter))
+
+        self.iter = self.start_iter
+
+        with EventStorage(self.start_iter) as self.storage:
+            try:
+                self.before_train()
+                for self.iter in range(self.start_iter, self.max_iter):
+                    self.before_step()
+                    self.run_step()
+                    self.after_step()
+                    self.update_progress()
+                    if self.iter % 20 == 0:
+                        self.log_metrics({name.replace("@", "_"): value[0] for name, value in self.storage.latest().items()}, step=self.iter)
+                    if self.stop_train():
+                        logger.info("Training stopped by user at iteration {}".format(self.iter))
+                        with open(os.path.join(self.output_dir, "model_final.pth"), "w") as f:
+                            f.write("")
+                        self.checkpointer.save("model_final")
+                        break
+                # self.iter == max_iter can be used by `after_train` to
+                # tell whether the training successfully finished or failed
+                # due to exceptions.
+                self.iter += 1
+            except Exception:
+                logger.exception("Exception during training:")
+                raise
+            finally:
+                self.after_train()
+        if len(self.cfg.TEST.EXPECTED_RESULTS) and comm.is_main_process():
+            assert hasattr(
+                self, "_last_eval_results"
+            ), "No evaluation results obtained during training!"
+            verify_results(self.cfg, self._last_eval_results)
+            return self._last_eval_results
+
+
+class MyMapper(DatasetMapper):
+    def __init__(
+            self,
+            is_train: bool,
+            augmentations: List[Union[T.Augmentation, T.Transform]],
+            image_format: str,
+            keypoint_hflip_indices=None):
+        # fmt: off
+        self.is_train = is_train
+        self.augmentations = T.AugmentationList(augmentations)
+        self.image_format = image_format
+        self.keypoint_hflip_indices = keypoint_hflip_indices
+        # fmt: on
+        logger = logging.getLogger(__name__)
+        mode = "training" if is_train else "inference"
+        logger.info(f"[DatasetMapper] Augmentations used in {mode}: {augmentations}")
+
+    def __call__(self, dataset_dict):
+        """
+        Args:
+            dataset_dict (dict): Metadata of one image, in Detectron2 Dataset format.
+        Returns:
+            dict: a format that builtin models in detectron2 accept
+        """
+        dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
+        # can use other ways to read image
+        image = utils.read_image(dataset_dict["file_name"], format="BGR")
+        # See "Data Augmentation" tutorial for details usage
+        auginput = T.AugInput(image)
+        transform = self.augmentations(auginput)
+        image = torch.from_numpy(auginput.image.transpose(2, 0, 1).copy())
+        img_shape = image.shape[1:]
+
+        annos = [
+            utils.transform_instance_annotations(annotation, [transform], image.shape[1:])
+            for annotation in dataset_dict.pop("annotations")
+        ]
+
+        return {
+            # create the format that the model expects
+            "image": image,
+            "instances": utils.annotations_to_instances(annos, img_shape),
+            "width": img_shape[1],
+            "height": img_shape[0]
+        }
+
+
+def register_dataset(dataset_name, images, metadata):
+    DatasetCatalog.register(dataset_name, lambda: images)
+    MetadataCatalog.get(dataset_name).thing_dataset_id_to_contiguous_id = metadata["thing_dataset_id_to_contiguous_id"]
+
+    MetadataCatalog.get(dataset_name).thing_classes = metadata["thing_classes"]
+
+
+
+
+def register_datasets(data, split):
+    category_names = data["metadata"]["category_names"]
+    thing_classes = [v for k, v in sorted(category_names.items(), key=lambda item: item[0])]
+    """thing_dataset_id_to_contiguous_id = None
+    if not (min(category_names) == 1 and max(category_names) == len(category_names)):"""
+    id_map = {v: i for i, v in enumerate(category_names)}
+    thing_dataset_id_to_contiguous_id = id_map
+    for sample in data["images"]:
+        if "file_name" not in sample.keys():
+            sample["file_name"] = sample["filename"]
+        for anno in sample["annotations"]:
+            anno["bbox_mode"] = BoxMode.XYWH_ABS
+            if thing_dataset_id_to_contiguous_id is not None:
+                anno["category_id"] = thing_dataset_id_to_contiguous_id[anno["category_id"]]
+    random.seed(10)
+    random.shuffle(data["images"])
+    split_id = int(len(data["images"]) * split)
+    train_imgs = data["images"][:split_id]
+    test_imgs = data["images"][split_id:]
+    DatasetCatalog.clear()
+    MetadataCatalog.clear()
+    data["metadata"]["thing_classes"] = thing_classes
+    data["metadata"]["thing_dataset_id_to_contiguous_id"] = thing_dataset_id_to_contiguous_id
+    register_dataset("TrainDetectionDataset", train_imgs, data["metadata"])
+    register_dataset("TestDetectionDataset", test_imgs, data["metadata"])
+    random.seed(0)
