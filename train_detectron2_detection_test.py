056999ba5f4e273e0815f71d72413314247c38b8
ambroise <ambroise.berthe@ikomia.com>
2023-08-07 15:16:49 +0200

    Add test file

---
+++ train_detectron2_detection_test.py
@@ -0,0 +1,17 @@
+import logging
+from ikomia.utils.tests import run_for_test
+
+logger = logging.getLogger(__name__)
+
+
+def test(t, data_dict):
+    logger.info("===== Test::train detectron2 detection =====")
+    input_dataset = t.get_input(0)
+    params = t.get_parameters()
+    params["batch_size"] = "2"
+    params["max_iter"] = "10"
+    params["dataset_split_ratio"] = "0.5"
+    params["eval_period"] = "3"
+    t.set_parameters(params)
+    input_dataset.load(data_dict["datasets"]["detection"]["dataset_coco"])
+    yield run_for_test(t)
